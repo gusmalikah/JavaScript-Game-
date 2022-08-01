9eae28bc5a85ec239272ce6ce6336d7ebd011917
unknown <hannanmalik1999@gmail.com>
2022-08-02 01:22:41 +0200

    Adding more Monsters

--- data.js
+++ data.js
@@ -1,16 +1,30 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "images/wizard.png",
+        avatar: "../images/wizard.png",
         health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
-    monster: {
+    orc: {
         name: "Orc",
-        avatar: "images/orc.png",
-        health: 40,
+        avatar: "../images/orc.png",
+        health: 30,
         diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "../images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "../images/goblin.png",
+        health: 20,
+        diceCount: 3,
         currentDiceScore: []
     }
 }
